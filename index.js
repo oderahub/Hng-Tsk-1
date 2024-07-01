c28466172c37825e1d925f3a1cd25477fd7fc8b8
oderahub <okere_chidera@yahoo.com>
2024-07-01 15:27:51 +0100

    Initial project setup

---
+++ index.js
@@ -0,0 +1,59 @@
+import express from "express";
+import requestIP from "request-ip";
+import fetch from "node-fetch";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Tell Express to trust the first proxy in front of it
+app.set("trust proxy", true);
+
+// Use request-ip middleware
+app.use(requestIP.mw());
+
+app.get("/api/hello", async (req, res) => {
+  const visitorName = req.query.visitor_name;
+  const clientIp = req.clientIp;
+  const apiKey = process.env.WEATHER_API_KEY;
+
+  try {
+    // Fetch weather data based on client IP
+    const weatherResponse = await fetch(
+      `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${clientIp}`
+    );
+
+    console.log(`Weather API Response Status: ${weatherResponse.status}`); // Debug: Log response status
+
+    if (!weatherResponse.ok) {
+      const errorText = await weatherResponse.text(); // Get the response text
+      console.error(
+        `Failed to fetch weather data. Status: ${weatherResponse.status}, Error: ${errorText}`
+      );
+      throw new Error("Failed to fetch weather data.");
+    }
+
+    const weatherData = await weatherResponse.json();
+    console.log(`Weather Data: ${JSON.stringify(weatherData)}`); // Debug: Log the weather data
+
+    const temperature = weatherData.current.temp_c; // Temperature in Celsius
+    const location = weatherData.location.name; // City name
+
+    const greeting = `Hello, ${visitorName}!, the temperature is ${temperature} degrees Celsius in ${location}`;
+
+    res.json({
+      client_ip: clientIp,
+      location: location,
+      greeting: greeting,
+    });
+  } catch (error) {
+    console.error("Error fetching weather data:", error);
+    res.status(500).send("Failed to fetch weather data.");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
